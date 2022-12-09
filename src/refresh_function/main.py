18c2345ecaf5638a7132f7cf9e282cc03f83de42
Nina Schiff <280141+ninas@users.noreply.github.com>
2022-12-08 21:25:37 -0800

    Add cloud function to poll polar api and combine with new sources from db

---
+++ src/refresh_function/main.py
@@ -0,0 +1,34 @@
+import structlog
+
+from src.utils import gcp_utils, log
+from polar import *
+
+def http(request, is_dev=False):
+    structlog.contextvars.bind_contextvars(run_reason="refresh")
+    log.config_structlog(is_dev)
+
+    logger = structlog.get_logger()
+    logger.info("Starting refresh run")
+    api = PolarAPI(logger)
+    data = api.exercises
+
+    if len(data) > 0:
+        p = Process(logger)
+        workouts = p.map_data(data)
+
+        logger.debug(
+            "Workouts to be saved",
+            workouts= [workout.as_dict() for workout in workouts]
+        )
+
+        p.save_to_db(workouts)
+    else:
+        logger.info("No workouts returned by the API",
+                    api_response=api)
+
+    logger.info("Completed run")
+    return ""
+
+
+if __name__ == "__main__":
+    http(None, True)
--- src/refresh_function/polar.py
+++ src/refresh_function/polar.py
@@ -1,121 +1,114 @@
 from peewee import fn
 from playhouse.db_url import connect
 from datetime import timedelta
-from functools import cache
+from functools import cache, cached_property
 
-from src.utils import gcp_utils
+from src.utils import gcp_utils, log
 from src.db.sources import models as source_models
 from src.db.workout import models as workout_models
 from src.db.workout.workout import Workout
 
 from src.polar_api.polar_api import PolarAPI
 
-UNIX_SOCKET_PATH = "/cloudsql/"
+
+class Process:
+    UNIX_SOCKET_PATH = "/cloudsql/"
+    def __init__(self, logger):
+        self.logger = logger
+
+    @cache
+    def database_connection_data(self, prefix):
+        return [gcp_utils.fetch_config(f"{prefix}/db_name")], {
+            "host": self.UNIX_SOCKET_PATH + gcp_utils.fetch_config(f"{prefix}/gcp_path"),
+            "user": gcp_utils.fetch_config(f"{prefix}/username"),
+            "password": gcp_utils.get_secret("db_workout"),
+        }
+
+    @cache
+    def _connect_to_db(self, name, model):
+        db_name, connection_info = self.database_connection_data(name)
+        self.logger.debug(
+            f"Opening connection to DB {name}",
+            db={
+                "name": db_name[0],
+                "host": connection_info["host"],
+                "user": connection_info["user"]
+            }
+        )
+        model.database.init(db_name[0], **connection_info)
+        model.database.connect()
+        return model.database
+
+    @cached_property
+    def source_db(self):
+        return self._connect_to_db("sources_db", source_models)
 
 
-@cache
-def source_db():
-    source_models.database.init(
-        gcp_utils.fetch_config("sources_db/db_name"),
-        host=UNIX_SOCKET_PATH + gcp_utils.fetch_config("sources_db/gcp_path"),
-        user=gcp_utils.fetch_config("sources_db/username"),
-        password=gcp_utils.get_secret("db_workout"),
-    )
-    source_models.database.connect()
-    return source_models.database
+    @cached_property
+    def workout_db(self):
+        return self._connect_to_db("workout_db", workout_models)
 
 
-@cache
-def workout_db():
-    print("in db func")
-    workout_models.database.init(
-        gcp_utils.fetch_config("workout_db/db_name"),
-        host=UNIX_SOCKET_PATH + gcp_utils.fetch_config("workout_db/gcp_path"),
-        user=gcp_utils.fetch_config("workout_db/username"),
-        password=gcp_utils.get_secret("db_workout"),
-    )
-    workout_models.database.connect()
-    print("connected")
-    return workout_models.database
+    def get_sources(self, earliest):
+        with self.source_db.atomic():
+            results = (
+                source_models.SourceInput.select()
+                .where(source_models.SourceInput.created >= earliest)
+                .execute()
+            )
+        return results
 
 
-def get_sources(earliest):
-    results = (
-        source_models.SourceInput.select()
-        .where(source_models.SourceInput.created >= earliest)
-        .execute()
-    )
-    print(results)
-    for i in results:
-        print(i)
-    return results
+    def get_last_saved_workout_date(self):
+        with self.workout_db.atomic():
+            return workout_models.Workouts.select(
+                fn.MAX(workout_models.Workouts.starttime)
+            ).scalar()
 
 
-def get_last_saved_workout_date():
-    print("last saved")
-    db = workout_db()
-    print(db)
-    with db.atomic():
-        return workout_models.Workouts.select(
-            fn.MAX(workout_models.Workouts.starttime)
-        ).scalar()
+    def map_data(self, data):
+        # For each source, we want the workout where it was created after the start time, but before the start time of the next workout
+        sources = self.get_sources(data[0].start_time)
+
+        self.logger.info("Mapping workouts to sources",
+                        workouts=[i.as_dict_for_logging() for i in data],
+                        sources=[src.as_dict() for src in sources]
+        )
+        s_i = 0
+        max_start = self.get_last_saved_workout_date()
+        self.logger.info(f"Last saved workout: {max_start}")
+
+        updated_workouts = []
+        for workout in data:
+            if max_start >= workout.start_time:
+                continue
+            added = False
+            for i, source in enumerate(sources[s_i:]):
+                if (
+                    workout.start_time < source.created
+                    and abs(source.created - workout.end_time) < timedelta(minutes=5)
+                    and (
+                        len(sources) == i + 1 or workout.start_time < sources[i + 1].created
+                    )
+                ):
+                    workout.add_source(source)
+                    updated_workouts.append(workout)
+                    s_i = i + 1
+                    added = True
+                    self.logger.info(
+                        "Found a match",
+                        workout=workout.as_dict(),
+                        source=source.as_dict(),
+                    )
+                    break
+
+            if not added:
+                self.logger.info("No match found", workout=workout.as_dict())
+
+        return updated_workouts
 
 
-def map_data(data, sources):
-    # For each source, we want the workout where it was created after the start time, but before the start time of the next workout
-
-    print("starting mapping")
-    s_i = 0
-    max_start = get_last_saved_workout_date()
-    print("last saved")
-    updated_workouts = []
-    for workout in data:
-        print("start loop")
-        if max_start >= workout.start_time:
-            print(f"skipping {workout}")
-            continue
-        added = False
-        for i, source in enumerate(sources[s_i:]):
-            print("enumerating sources")
-            if (
-                workout.start_time < source.created
-                and abs(source.created - workout.end_time) < timedelta(minutes=5)
-                and (
-                    len(sources) == i + 1 or workout.start_time < sources[i + 1].created
-                )
-            ):
-                print("in if")
-                workout.add_source(source)
-                updated_workouts.append(workout)
-                s_i = i + 1
-                added = True
-                print(f"Matched!")
-                print(f"Workout: {workout}")
-                print(f"Source: {source}")
-                break
-
-        if not added:
-            print(f"didn't find a match: {workout}")
-
-    print(updated_workouts)
-    return updated_workouts
-
-
-def save_to_db(workouts):
-    for workout in workouts:
-        print("starting insert")
-        w = Workout(workout_db(), workout)
-        w.insert_row()
-        print("row inserted")
-        break
-
-
-if __name__ == "__main__":
-    source_db()
-    api = PolarAPI()
-    print(api)
-    data = api.exercises
-    if len(data) > 0:
-        srcs = get_sources(data[0].start_time)
-        workouts = map_data(data, srcs)
-        save_to_db(workouts)
+    def save_to_db(self, workouts):
+        for workout in workouts:
+            w = Workout(self.workout_db, workout)
+            w.insert_row()
