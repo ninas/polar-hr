18c2efb3be5250b4716a7339646eabf6f6d94003
Nina Schiff <280141+ninas@users.noreply.github.com>
2022-12-09 01:44:37 -0800

    [base model] Simplify model printing

--- src/db/base_model.py
+++ src/db/base_model.py
@@ -8,24 +8,14 @@ class BaseModel(Model):
     def as_dict(self):
         fields = {}
         for field in self._meta.fields.items():
-            fields[field[0]] = {
-                "type": str(field[1]),
-                "value": getattr(self, field[0])
-            }
+            fields[field[0]] = getattr(self, field[0])
         return fields
 
 
     def __str__(self):
-        fields = [f"\t{name} {details['type']}: {details['value']}"
+        fields = [f"\t{name}: {details}"
                   for name, details in self.as_dict().items()]
 
         fields_str = "\n".join(fields)
 
         return f"{self.__class__.__name__}\n({fields_str})"
-
-    def as_json(self):
-        vals  = self.as_dict()
-        for k,v in vals.items():
-            if isinstance(v, datetime):
-                vals[k] = str(v)
-        return json.dumps(vals)
