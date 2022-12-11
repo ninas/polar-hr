302001f4eddec43b1c571a999b4baa44453ae2fa
Nina Schiff <280141+ninas@users.noreply.github.com>
2022-12-10 16:57:06 -0800

    [api] Add call to return all tags + cloud function backend

---
+++ src/api/tags/main.py
@@ -0,0 +1,25 @@
+from src.db.workout import models
+from src.utils.db_utils import DBConnection
+from src.utils import log
+from flask import make_response
+import json
+
+
+
+def tags_http(request, is_dev=False):
+    """
+    /tags
+
+    """
+    logger = log.new_logger(is_dev=is_dev)
+    logger.bind(path="/workouts")
+    db = DBConnection(logger).workout_db
+    with db.atomic():
+        tags = models.Tags.select().execute()
+
+    return_data = [tag.as_dict() for tag in tags]
+
+    return json.dumps(return_data)
+
+if __name__ == "__main__":
+    print(tags_http(None, True))
