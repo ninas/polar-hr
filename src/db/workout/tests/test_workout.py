3f341167ff889f7ef712466e6a7b777674397dec
Nina Schiff <280141+ninas@users.noreply.github.com>
2022-12-19 03:11:35 -0800

    [db][tests] Update workout tests

--- src/db/workout/tests/test_workout.py
+++ src/db/workout/tests/test_workout.py
@@ -2,99 +2,111 @@ import unittest
 from collections import defaultdict
 from datetime import timedelta
 from unittest.mock import MagicMock, Mock, PropertyMock, patch
+from copy import deepcopy
+import json
 
-from workout import Workout
+from src.utils.test_base import TestBase
+from src.db.workout.workout import Workout
+from src.db.workout.workout_data_store import WorkoutDataStore
 
-import db.models as models
+import src.db.workout.models as models
 
 
-class TestWorkout(unittest.TestCase):
+class TestWorkout(TestBase):
     def setUp(self):
         self.db = MagicMock()
-        self.data = self.data()
-        self.workout = Workout(self.db, self.data)
-        self.workout._find = MagicMock(return_value=None)
+        self.raw_data = self.data()
+        self.data = WorkoutDataStore(self.raw_data)
+        self.workout = Workout(self.db, self.data, self.logger)
+        self.workout.equipment = self._mock_equipment()
+        self.workout._insert_tags = MagicMock(side_effect=lambda a, b: a)
+        Workout._find = MagicMock(return_value=None)
 
     def data(self):
-        return {
-            "duration": "PT100S",
-            "startTime": "2022-10-31 22:25:13+00:00",
-            "exercises": [
-                {
-                    "zones": {
-                        "heart_rate": [
-                            {
-                                "lowerLimit": 103,
-                                "higherLimit": 124,
-                                "inZone": "PT10S",
-                                "zoneIndex": 1,
-                            },
-                            {
-                                "lowerLimit": 124,
-                                "higherLimit": 144,
-                                "inZone": "PT20S",
-                                "zoneIndex": 2,
-                            },
-                            {
-                                "lowerLimit": 144,
-                                "higherLimit": 165,
-                                "inZone": "PT30S",
-                                "zoneIndex": 3,
-                            },
-                            {
-                                "lowerLimit": 165,
-                                "higherLimit": 185,
-                                "inZone": "PT20S",
-                                "zoneIndex": 4,
-                            },
-                            {
-                                "lowerLimit": 185,
-                                "higherLimit": 206,
-                                "inZone": "PT10S",
-                                "zoneIndex": 5,
-                            },
-                        ]
-                    },
-                    "samples": ["something"],
-                }
-            ],
-        }
+        with open("src/db/workout/tests/sample_data.json") as f:
+            return json.loads(f.read())
 
-    def test_if_in_db_skip(self):
-        # Let's mock this so we can see if it's reached
-        self.workout._find.return_value = "something"
-        self.workout._populate_model = MagicMock()
-        self.workout._find = MagicMock(return_value="something")
-        self.workout._populate_model.assert_not_called()
+    def test_exit_early(self):
+        pop = MagicMock()
 
-    @patch("workout.upload_to_cloud_storage")
-    def test_fail_upload_blob(self, mock_cloud_storage):
-        mock_cloud_storage.side_effect = Exception("something")
-        self.assertRaises(Exception, self.workout.insert_row)
+        def _create(dd):
+            wd = WorkoutDataStore(dd)
+            wrk = Workout(self.db, wd, self.logger)
+            # Let's mock this so we can see if it's reached
+            wrk._populate_model = pop
+            return wrk
 
-    def test_missing_data(self):
-        del self.workout.data["exercises"][0]["samples"]
-        self.assertEqual(self.workout.insert_row(), None)
+        d = deepcopy(self.raw_data)
+        del d["start_time"]
+        w = _create(d)
+        self.assertEqual(w.insert_row(), None)
+        pop.assert_not_called()
 
-    @patch("workout.models.Workouts")
-    @patch("workout.upload_to_cloud_storage")
-    @patch("workout.Source.load_source")
-    def test_failed_insert(self, mock_src, mock_cloud_store, mock_workout_model):
-        self.workout.note_data = {"sources": ["something"]}
-        self.workout._populate_model = MagicMock()
-        # If we error inserting a row, print info and move on
-        mock_cloud_store.return_value = "something"
-        mock_src.side_effect = Exception("oops")
+        d = deepcopy(self.raw_data)
+        del d["samples"]
+        w = _create(d)
+        self.assertEqual(w.insert_row(), None)
+        pop.assert_not_called()
 
-        try:
-            self.workout.insert_row()
-        except Exception as e:
-            self.fail("Workout.insert_row raised an unexpected exception")
+        d = deepcopy(self.raw_data)
+        w = _create(d)
+        Workout._find = MagicMock(return_value="something")
+
+        self.assertEqual(w.insert_row(), "something")
+        pop.assert_not_called()
+
+    def _mock_equipment(self):
+        equipment_mocks = []
+        for i in self.data.equipment["weights"]:
+            equipment_mocks.append(
+                MagicMock(
+                    equipmenttype=models.EquipmentType.WEIGHTS,
+                    magnitude=i["magnitude"],
+                    quantity=i["quantity"],
+                )
+            )
+        return equipment_mocks
+
+    def test_tags_created(self):
+        tgs = MagicMock()
+        self.workout._populate_model = MagicMock(
+            return_value=MagicMock(tags=MagicMock(add=tgs))
+        )
+        self.data._i_data["sources"] = []
+        self.workout._insert_samples = MagicMock()
+        self.workout._insert_hr_zones = MagicMock()
+        self.workout.insert_row()
+        self.assertEqual(
+            set(*(tgs.call_args.args)), {"hiit", "5lbs", "8lbs", "weights",}
+        )
+
+    def test_equipment_to_tags(self):
+        self.assertEqual(
+            self.workout._equipment_to_tags(), {"5lbs", "8lbs", "weights",}
+        )
+
+        equip = self._mock_equipment()
+        equip.append(
+            MagicMock(
+                equipmenttype=models.EquipmentType.BANDS, magnitude="heavy", quantity=1,
+            )
+        )
+        self.workout = Workout(self.db, self.data, self.logger)
+        self.workout.equipment = equip
+        self.assertEqual(
+            self.workout._equipment_to_tags(),
+            {"5lbs", "8lbs", "weights", "heavy band", "mini-band"},
+        )
+
+        self.workout = Workout(self.db, self.data, self.logger)
+        self.workout.equipment = []
+        self.assertEqual(self.workout._equipment_to_tags(), set())
 
     def test_create_hr_zones(self):
-        workout = MagicMock()
-        with patch("workout.models.HRZones") as hrzones:
-            zones = self.workout._create_hr_zones(workout)
+        self.workout.model = MagicMock()
+        self.data._i_data["duration"] = "PT100S"
+        with patch("src.db.workout.workout.models.HRZones") as hrzones:
+            zones = self.workout._insert_hr_zones()
 
         self.assertEqual(
             zones[0],
@@ -104,7 +116,7 @@ class TestWorkout(unittest.TestCase):
                 "higherlimit": 206,
                 "duration": timedelta(seconds=10),
                 "percentspentabove": 10,
-                "workout": workout,
+                "workout": self.workout.model,
             },
         )
         self.assertEqual(
@@ -115,66 +127,6 @@ class TestWorkout(unittest.TestCase):
                 "higherlimit": 103,
                 "duration": timedelta(seconds=10),
                 "percentspentabove": 100,
-                "workout": workout,
+                "workout": self.workout.model,
             },
         )
-
-    def test_equipment_gen(self):
-        self.workout.note_data = {}
-        self.assertEqual(self.workout.equipment, [])
-
-        self.workout._equipment = None
-        self.workout.note_data = {
-            "equipment": {
-                models.EquipmentType.WEIGHTS: [{"magnitude": "10", "quantity": 1}]
-            }
-        }
-
-        with patch("workout.models.Equipment") as mock_equip:
-            mock_equip.get_or_create = MagicMock(return_value=(MagicMock(), True))
-            self.assertEqual(len(self.workout.equipment), 1)
-
-    def test_parse_note(self):
-
-        empty_dict = defaultdict(list)
-        vid1 = "www.vid1.com"
-        vid2 = "www.vid2.com"
-        vid3 = "www.vid3.com"
-        val_to_response = {
-            "": {},
-            None: {},
-            f"Multiple videos: {vid1} , {vid2} , {vid3}": {
-                "sources": [vid1, vid2, vid3],
-                "equipment": empty_dict,
-            },
-            f"{vid1} ; weights: 1,2,3, one 4": {
-                "sources": [vid1],
-                "equipment": defaultdict(
-                    list,
-                    {
-                        models.EquipmentType.WEIGHTS: [
-                            {"quantity": 2, "magnitude": "1"},
-                            {"quantity": 2, "magnitude": "2"},
-                            {"quantity": 2, "magnitude": "3"},
-                            {"quantity": 1, "magnitude": "4"},
-                        ]
-                    },
-                ),
-            },
-            f"{vid1} ; weights:": {"sources": [vid1], "equipment": empty_dict},
-            f"Multiple videos: {vid1} , {vid2} ; bands: medium,heavy ; note: nothing": {
-                "sources": [vid1, vid2],
-                "equipment": defaultdict(
-                    list,
-                    {
-                        models.EquipmentType.BANDS: [
-                            {"quantity": 1, "magnitude": "medium"},
-                            {"quantity": 1, "magnitude": "heavy"},
-                        ]
-                    },
-                ),
-            },
-        }
-
-        for k, v in val_to_response.items():
-            self.assertEqual(self.workout._parse_note(k), v)
