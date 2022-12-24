5c04f02f6f70bcb9b3e659fcdddf194938943505
Nina Schiff <280141+ninas@users.noreply.github.com>
2022-12-24 13:02:00 -0800

    [api_functions] Create single serverless service for all simple GETs

---
+++ src/api_functions/fetchall/main.py
@@ -0,0 +1,33 @@
+import json
+
+
+
+def equipment_http(request):
+    """
+    /equipment
+    """
+    pass
+
+
+def tags_http(request, is_dev=False):
+    """
+    /tags
+
+    """
+    pass
+
+
+def exercises_http(request, is_dev=False):
+    pass
+
+
+def everything_http(request, is_dev=False):
+    """
+    /everything
+
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
